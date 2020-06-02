4912dac6563b971f68fa83379a8509f8cfcfe1ea
Andrey <firebird_07@mail.ru>
2020-06-02 21:38:38 +0300

    Optimization

--- webpack.config.js
+++ webpack.config.js
@@ -7,18 +7,56 @@ const Autoprefixer = require('autoprefixer');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserWebpackPlugin = require('terser-webpack-plugin');
 
+const DevMode = process.env.NODE_ENV === 'development';
+const ProdMode = !DevMode;
 
+console.log('DevMode: ', DevMode);
 
-const optimization = () => {
+function optimization() {
     const config = {
         splitChunks: {
             chunks: 'all'
         }
     };
+
+    if (ProdMode) {
+        config.minimizer = [
+            new OptimizeCssAssetsPlugin(),
+            new TerserWebpackPlugin()
+        ]
+    }
     return config;
-};
+}
 
+function fileName(extention) {
+    if (DevMode){
+        return `[name].${extention}`
+    } else {
+        return `[name].[hash].${extention}`
+    }
+}
 
+// function addPlugin(newPlugin) {
+//     const prodPlugin = [];
+//
+//     if (ProdMode && newPlugin) {
+//         prodPlugin.push(newPlugin);
+//     }
+//
+//     return prodPlugin;
+// }
+
+function cssLoaders() {
+    let currentMode = [
+        MiniCssExtractPlugin.loader,
+        'css-loader'
+    ];
+    const productionMode = {loader: 'postcss-loader', options: {plugins: [new Autoprefixer()]}};
+    if (ProdMode) {
+        currentMode = [...currentMode, productionMode]
+    }
+    return currentMode;
+}
 
 module.exports = {
     mode: 'development',
@@ -29,25 +67,29 @@ module.exports = {
         }
     },
     entry: {
-        main: './js/app.js',
+        main: ['@babel/polyfill', './js/app.js'],
         analytics: './js/analytics.js'
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[hash].js'
+        filename: fileName('js')
     },
     // watch: false,
     // watchOptions: {
     //     aggregateTimeout: 100
     // },
-    devtool: 'source-map',
+    devtool: DevMode ? 'source-map' : '',
     optimization: optimization(),
     devServer: {
-        port: 4200
+        port: 4200,
+        hot: DevMode
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './index.html'
+            template: './index.html',
+            minify: {
+                collapseWhitespace: ProdMode
+            }
         }),
         new CleanWebpackPlugin(),
         new CopyWebPackPlugin({
@@ -59,18 +101,16 @@ module.exports = {
             ]
         }),
         new MiniCssExtractPlugin({
-            filename: '[name].[hash].css'
+            filename: fileName('css'),
+            hmr: DevMode,
+            reloadAll: true
         }),
     ],
     module: {
         rules: [
             {
                 test: /\.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    {loader: 'postcss-loader', options: {plugins: [Autoprefixer()]}},
-                ]
+                use: cssLoaders()
             },
             {
                 test:/\.(jpg|png|svg|gif)$/,
@@ -83,7 +123,14 @@ module.exports = {
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: 'babel-loader'
+                loader: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env'
+                        ]
+                    }
+                }
             }
         ],
     },
